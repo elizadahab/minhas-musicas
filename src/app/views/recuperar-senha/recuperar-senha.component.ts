6ef7bf129f48fd31dc8bc80e760d8a9e0df08f39
Elizabeth Dahab <elizabethdahab@Mac-mini-de-Elizabeth.local>
2026-03-20 09:41:26 -0300

    feat: telas de login, cadastro e recuperar senha

--- src/app/views/recuperar-senha/recuperar-senha.component.ts
+++ src/app/views/recuperar-senha/recuperar-senha.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recuperar-senha',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './recuperar-senha.component.html',
-  styleUrl: './recuperar-senha.component.scss'
+  styleUrls: ['./recuperar-senha.component.scss']
 })
 export class RecuperarSenhaComponent {
+  form: FormGroup;
+  carregando = false;
+  enviado = false;
+  erro = '';
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  enviar() {
+    if (this.form.invalid) return;
+    this.carregando = true;
+    this.erro = '';
+
+    this.authService.recuperarSenha(this.form.value.email).subscribe({
+      next: () => {
+        this.enviado = true;
+        this.carregando = false;
+      },
+      error: () => {
+        this.erro = 'Email não encontrado. Verifique e tente novamente.';
+        this.carregando = false;
+      }
+    });
+  }
 }
